c9cf0df58fd7d4c6edb20f0d8a9266ef3d78a482
Mafe Bracho <mafe.bracho@gmail.com>
2021-05-04 16:08:53 +0200

    first commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by María Fernanda Bracho Salazar on 26.08.20.
+//  Copyright © 2020 María Fernanda Bracho Salazar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
